ab343264f81bd8b71cdbae6128cd908f7025d8b7
Vipin <vipinbhardwal286@gmail.com>
2026-01-30 01:14:44 +0530

    Resolve stash conflict in themeSlice

--- Redux/src/features/theme/themeSlice.js
+++ Redux/src/features/theme/themeSlice.js
@@ -10,23 +10,4 @@ const themeSlice = createSlice({
 });
 
 export const {setThemeLight, setThemeDark} = themeSlice.actions
-<<<<<<< Updated upstream
 export default themeSlice.reducer;
-
-=======
-export default themeSlice.reducer
->>>>>>> Stashed changes
-
-import { createSlice } from "@reduxjs/toolkit";
-
-const themeSlice = createSlice({
-    name: "theme",
-    initialState: "light",
-    reducers: {
-        setThemeLight: (state) => "light",
-        setThemeDark: (state) => "dark"
-    }
-});
-
-export const {setThemeLight, setThemeDark} = themeSlice.actions
-export default themeSlice.reducer
